4d8cf91be504c27dd096e0e2cc62537077112cd1
emilywheeler <emily.wheeler@icloud.com>
2023-01-30 21:48:12 +1100

    removed 'some' from the lets explore us data

--- bikeshare.py
+++ bikeshare.py
@@ -15,7 +15,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "None" to apply no month filter
         (str) day - name of the day of week to filter by, or "None" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Hello! Let\'s explore US bikeshare data!')
     # get user input for city (chicago, new york city, washington).
     while True:
         cities= ['chicago','new york city','washington']
